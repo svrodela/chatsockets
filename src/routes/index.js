9622dfecd2bab003589c05b259722cc4624da6d6
svrodela <svrodela@gmail.com>
2023-10-02 12:34:40 -0600

    se envia mensaje por metodo post

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
+const net = require('net');
+const readline = require('readline-sync');
+
+const servidor={
+    port:3000,
+    host: 'localhost'
+}
+const client = net.createConnection(servidor);
+    client.on('connect',()=>{
+        console.log('conexión satisfactoria')
+     //   sendLine()
+    })
+
 router.get('/', async (req, res) =>{
 
-    // console.log("hola");
+
    res.render('index.ejs');
 });
+
+router.post('/enviar', async (req, res) => {
+    const datos = req.body;
+    console.log("Mensaje de: ", datos.usuario,"  ", datos.mensaje);
+   client.write(datos.usuario + " " + datos.mensaje)
+    res.redirect('/');
+});
+
+
 module.exports = router;
